08f776e7af6ff54f317ec5c652c22440c5b42462
alefernandezv <107263289+alefernandezv@users.noreply.github.com>
2024-06-17 15:57:35 -0400

    V0.5

--- static/js/script_login.js
+++ static/js/script_login.js
@@ -1,21 +1,81 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+import {Container, Row , Button, Form, FormGroup} from 'reactstraps';
+ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+ import { initializeApp } from "firebase/app";
+ import { getAnalytics } from "firebase/analytics";
+ const provider= new GoogleAuthProvider();
+import mainstyles from '../css/styles_login.css'
+import{ fontAwesoneIcon} from "@fortAwesone/reacts-fontAweasome";
+import {
+    faFacebook,
+    faTwitter,
+    faGoogle
+} from "@fortawesome/fre-brands-svg-icons";
+import { sign } from 'crypto';
+import { error } from 'console';
+export default function Login(){
+    const firebaseConfig = {
+        apiKey: "AIzaSyD0-zFQ31iXpFiFfbqyhN1xXv4FkJ9Hrz4",
+        authDomain: "ferremas-a6d03.firebaseapp.com",
+        databaseURL: "https://ferremas-a6d03-default-rtdb.firebaseio.com",
+        projectId: "ferremas-a6d03",
+        storageBucket: "ferremas-a6d03.appspot.com",
+        messagingSenderId: "236006876919",
+        appId: "1:236006876919:web:61c358086739bb6aaf95f2"
+      };
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
+    const auth=getAuth();
 
-        // Obtener los valores del formulario
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
 
-        // Validar el inicio de sesión (aquí puedes implementar tu lógica de validación)
-        if (email === 'usuario@example.com' && password === 'contraseña') {
-            // Inicio de sesión correcto
-            // Redirigir a la página de inicio o realizar otras acciones
-            window.location.href = 'dashboard.html'; // Redirigir a la página de inicio después del inicio de sesión exitoso
-        } else {
-            // Inicio de sesión incorrecto
-            // Mostrar un mensaje de error
-            var errorMessage = document.getElementById('error-message');
-            errorMessage.textContent = 'Correo electrónico o contraseña incorrectos. Por favor, inténtalo de nuevo.';
-        }
-    });
-});
+    function call_login_google(){
+        signInWithPopup(auth,provider)
+        .then((result) => {
+            const credential= GoogleAuthProvider.credentialFromResult(result);
+            const token =credential.accessToken;
+
+            const user=result.user;
+
+        }).catch((error)=>{
+            const errorCode =error.code;
+            const errorMessage= error.message;
+            const email= error.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+
+        });
+
+
+    }
+
+    return(
+
+        <>
+            <div className={mainstyles.imagebackground}>
+                <div className={'container'+mainstyles.container}>
+                <div className={'row col-md-4 offset-md-4 d-flex align-items-center '+mainstyles.loginbox}>
+                    <div className={mainstyles.Row}>
+                        <h2 style={{TextAlign:'center'}}>Login</h2>
+
+
+                    </div>
+                    <div className={mainstyles.col}>
+                        <a href="# " className={'row'+mainstyles.fb+' '+mainstyles.btn}>
+                            <fontAwesoneIcon icon={faFacebook} tyle={{fontSize:100,color: "white"}}className={'col-2'}/>login Facebook
+                        </a>
+                        <a href="# " className={'row'+mainstyles.fb+' '+mainstyles.btn}>
+                            < fontAwesoneIcon icon={faTwitter} tyle={{fontSize:100,color: "white"}} className={'col-2'}/>login Twitter
+                        </a>
+                        <a href="# " className={'row'+mainstyles.fb+' '+mainstyles.btn} onClick={call_login_google}>
+                            < fontAwesoneIcon icon={faGoogle} tyle={{fontSize:100,color: "white"}} className={'col-2'}/>login Google
+                        </a>
+
+                    </div>
+                </div>
+
+                </div>
+                
+
+            </div>
+        
+        </>
+    )
+}
